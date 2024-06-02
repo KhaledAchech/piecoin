324522f22b23a243bca983d595e5a9b0c23c0008
KhaledAchech <achechkhaled@gmail.com>
2024-06-02 23:32:53 +0200

    genesis block and fixing encode string

--- piecoin_block.py
+++ piecoin_block.py
@@ -1,3 +1,4 @@
+import datetime as date
 import hashlib as hasher
 
 class Block:
@@ -10,9 +11,27 @@ class Block:
     
     def hash_bloc(self):
         sha = hasher.sha256()
-        sha.update(str(self.index)
-                 + str(self.timestamp)
-                 + str(self.data)
-                 + str(self.previous_hash))
+        to_hash = str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash)
+        sha.update(to_hash.encode())
         
         return sha.hexdigest()
+
+def create_genesis_block():
+    return Block(0, date.datetime.now(), "Genesis Block", "0")
+    
+def next_block(last_block):
+    this_index = last_block.index + 1
+    this_timestamp = date.datetime.now()
+    this_data = "Hey! I'm block " + str(this_index)
+    this_hash = last_block.hash
+    return Block(this_index, this_timestamp, this_data, this_hash)
+
+blockchain = [create_genesis_block()]
+previous_block = blockchain[0]
+
+for i in range(20):
+    block_to_add = next_block(previous_block)
+    blockchain.append(block_to_add)
+    previous_block = block_to_add
+    print ("Block #{} has been added to the blockchain!".format(block_to_add.index))
+    print ("Hash: {}\n".format(block_to_add.hash))
